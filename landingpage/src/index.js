f19eddf686dd7038dcfb02df74438cd624b30f5d
Steven Hanson <steven.hanson@wintechnologies.net>
2020-01-02 14:45:00 +0000

    Add grid

--- landingpage/src/index.js
+++ landingpage/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Container, Row, Col } from 'react-grid-system';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+    render() {
+        return (
+            <div className="section-a">
+                <Container>
+                    <Row>
+                        <Col lg={6}>
+                            1/2 grid here!
+                        </Col>
+                        <Col lg={6}>
+                            1/2 grid here!
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
